d989b886e39deda07a254e38ada59b5d4efb5959
dheeruz <akulasaidheeraj.cs2022@citchennai.net>
2026-05-16 18:37:42 +0530

    Fix TS2835: Add .js extension to relative imports in api routes for Vercel deployment

--- api/notify/signup.ts
+++ api/notify/signup.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { transporter, adminEmail } from '../_utils/transporter';
+import { transporter, adminEmail } from '../_utils/transporter.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
